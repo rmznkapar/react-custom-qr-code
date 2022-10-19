f089371acaaeaa3f403245534032838ba1d8f0e3
Ramazan Kapar <rmznkapar@gmail.com>
2022-10-19 12:00:51 +0300

    Update index.js

--- src/components/QRCodeCell/index.js
+++ src/components/QRCodeCell/index.js
@@ -4,15 +4,20 @@ import React from "react";
 const propTypes = {
   d: PropTypes.string.isRequired,
   fill: PropTypes.string.isRequired,
+  dot: PropTypes.bool,
+  size: PropTypes.number,
   transformX: PropTypes.number.isRequired,
   transformY: PropTypes.number.isRequired,
 };
 
 const defaultProps = {};
 
-const QRCodeCell = ({ d, fill, transformX, transformY }) => (
-  <path d={d} fill={fill} transform={`matrix(${[1, 0, 0, 1, transformX, transformY]})`} />
-);
+const QRCodeCell = ({ d, fill, transformX, transformY, dot }) => {
+  if (dot) {
+    return  <circle r={size}  fill={fill} transform={`matrix(${[1, 0, 0, 1, transformX, transformY]})`} />;
+  } 
+  return <path d={`M 0 0 L ${size} 0 L ${size} ${size} L 0 ${size} Z`} fill={fill} transform={`matrix(${[1, 0, 0, 1, transformX, transformY]})`} />;
+}
 
 QRCodeCell.propTypes = propTypes;
 QRCodeCell.defaultProps = defaultProps;
